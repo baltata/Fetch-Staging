f0268dfe575d415b83bc20d87c44fb232afebe91
baltata <128985806+baltata@users.noreply.github.com>
2023-09-18 14:10:36 +0200

    Update correctionpremium.js

--- correctionpremium.js
+++ correctionpremium.js
@@ -3,6 +3,6 @@ var contenairexercice = document.querySelectorAll(".exercice iframe");
 for (let iframe of contenairexercice) {
    iframe.addEventListener("load",() => {
   var correction =iframe.contentWindow.document.body.querySelector('.outcome');
-  correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});
+  correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="top" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});
   
 }
