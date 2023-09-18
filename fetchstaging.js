79038645ed986c0284d5b071adb505df91e7a792
baltata <128985806+baltata@users.noreply.github.com>
2023-09-18 15:02:20 +0200

    Update fetchstaging.js

--- fetchstaging.js
+++ fetchstaging.js
@@ -64,6 +64,7 @@ for (var i = 0, len = liste.length; i < len; i++) {
         console.log("le container exercice est :",contenairexercice)
           contenairexercice.addEventListener("load",() => {
   var correction =contenairexercice.contentWindow.document.body.querySelector('.outcome');
+              console.log("loutcome est :",correction);
   correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});
   
 }
