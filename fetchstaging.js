a729d88d31ab6491434d22b67759c842dca49bb9
baltata <128985806+baltata@users.noreply.github.com>
2023-09-18 16:08:22 +0200

    Update fetchstaging.js

--- fetchstaging.js
+++ fetchstaging.js
@@ -68,7 +68,7 @@ for (var i = 0, len = liste.length; i < len; i++) {
   var correction =iframe.contentWindow.document.body.querySelector('.outcome');
       console.log("le div de correction est :",correction);
    
-  correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});},1000)
+  correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});},3000)
   
 }
     }}
