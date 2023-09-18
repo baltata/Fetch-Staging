94ecf5b6406551b26e0af8501da2e9a8c721e9b5
baltata <128985806+baltata@users.noreply.github.com>
2023-09-18 16:48:57 +0200

    Update fetchstaging.js

--- fetchstaging.js
+++ fetchstaging.js
@@ -62,14 +62,13 @@ for (var i = 0, len = liste.length; i < len; i++) {
     if (isThePageBeingEdited!="page-mod-book-edit"){
         var iframe = liste[i].querySelector("iframe");
         console.log("le iframe est :",iframe);
-          var correction =iframe.contentWindow.document.body.querySelector('.outcome');
-        console.log("le div de correction est :",correction);
-        correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>');
-
+         
         
   iframe.addEventListener("load",() => {
 
-
+ var correction =iframe.contentWindow.document.body.querySelector('.outcome');
+        console.log("le div de correction est :",correction);
+     
    console.log("reload of the iframe");
   correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});
   
