501f63cbf3acbc8434924cbdc16c4603e9223496
baltata <128985806+baltata@users.noreply.github.com>
2023-10-05 14:16:13 +0200

    Delete correctionpremium.js

--- correctionpremium.js
+++
@@ -1,8 +0,0 @@
-console.log("DEBUT DU SSSS");
-var contenairexercice = document.querySelectorAll(".exercice iframe");
-for (let iframe of contenairexercice) {
-   iframe.addEventListener("load",() => {
-  var correction =iframe.contentWindow.document.body.querySelector('.outcome');
-  correction.insertAdjacentHTML('beforeend',' <div class="blur" style="display:grid"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')});
-  
-}
